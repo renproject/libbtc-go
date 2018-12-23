fe5b5ac3593e1cec761a43e8227dd5584c33f5bf
Susruth  Nadimpalli <susruth@republicprotocol.com>
2018-12-23 14:34:31 +1100

    clients: add bitcoin fullnode client

---
+++ clients/bitcoin_node.go
@@ -0,0 +1,189 @@
+package clients
+
+import (
+	"context"
+	"encoding/hex"
+	"fmt"
+	"math"
+
+	"github.com/btcsuite/btcd/chaincfg"
+	"github.com/btcsuite/btcd/chaincfg/chainhash"
+	"github.com/btcsuite/btcd/rpcclient"
+	"github.com/btcsuite/btcd/wire"
+	"github.com/btcsuite/btcutil"
+)
+
+type bitcoinFNClient struct {
+	client  *rpcclient.Client
+	client2 RPCCLient
+	params  *chaincfg.Params
+}
+
+func NewBitcoinFNClient(host, user, password string) (Client, error) {
+	core, err := NewBitcoinFNClientCore(host, user, password)
+	if err != nil {
+		return nil, err
+	}
+	return NewClient(core), nil
+}
+
+func NewBitcoinFNClientCore(host, user, password string) (ClientCore, error) {
+	client, err := rpcclient.New(
+		&rpcclient.ConnConfig{
+			Host:         host,
+			User:         user,
+			Pass:         password,
+			HTTPPostMode: true,
+			DisableTLS:   true,
+		},
+		nil,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	bcInfo, err := client.GetBlockChainInfo()
+	if err != nil {
+		return nil, err
+	}
+
+	var params *chaincfg.Params
+	switch bcInfo.Chain {
+	case "main":
+		params = &chaincfg.MainNetParams
+	case "test":
+		params = &chaincfg.TestNet3Params
+	case "regtest":
+		params = &chaincfg.RegressionNetParams
+	default:
+		return nil, fmt.Errorf("unsupported bitcoin network: %s", bcInfo.Chain)
+	}
+
+	return &bitcoinFNClient{
+		client:  client,
+		client2: NewRPCClient(host, user, password),
+		params:  params,
+	}, nil
+}
+
+func (client *bitcoinFNClient) GetUTXOs(ctx context.Context, address string, limit, confitmations int64) ([]UTXO, error) {
+	net := client.NetworkParams()
+	addr, err := btcutil.DecodeAddress(address, net)
+	if err != nil {
+		return []UTXO{}, err
+	}
+
+	unspents, err := client.client.ListUnspentMinMaxAddresses(0, 999999, []btcutil.Address{addr})
+	if err != nil {
+		return []UTXO{}, err
+	}
+
+	if len(unspents) == 0 {
+		if err := client.client.ImportAddressRescan(address, "", false); err != nil {
+			return []UTXO{}, err
+		}
+
+		unspents, err = client.client.ListUnspentMinMaxAddresses(0, 999999, []btcutil.Address{addr})
+		if err != nil {
+			return []UTXO{}, err
+		}
+	}
+
+	utxos := []UTXO{}
+	for _, unspent := range unspents {
+		utxos = append(utxos, UTXO{
+			TxHash:       unspent.TxID,
+			Amount:       int64(unspent.Amount * math.Pow(10, 8)),
+			ScriptPubKey: unspent.ScriptPubKey,
+			Vout:         unspent.Vout,
+		})
+	}
+	return utxos, nil
+}
+
+func (client *bitcoinFNClient) Confirmations(ctx context.Context, txHashStr string) (int64, error) {
+	txHash, err := chainhash.NewHashFromStr(txHashStr)
+	if err != nil {
+		return 0, err
+	}
+	tx, err := client.client.GetTransaction(txHash)
+	if err != nil {
+		return 0, err
+	}
+	return tx.Confirmations, nil
+}
+
+func (client *bitcoinFNClient) ScriptFunded(ctx context.Context, address string, value, conf int64) (bool, int64, error) {
+	if err := client.client.ImportAddressRescan(address, "scripts", false); err != nil {
+		return false, value, err
+	}
+	net := client.NetworkParams()
+	addr, err := btcutil.DecodeAddress(address, net)
+	if err != nil {
+		return false, value, err
+	}
+	amount, err := client.client.GetReceivedByAddressMinConf(addr, int(conf))
+	if err != nil {
+		return false, value, err
+	}
+	return int64(amount.ToUnit(btcutil.AmountSatoshi)) >= value, int64(amount.ToUnit(btcutil.AmountSatoshi)), nil
+}
+
+func (client *bitcoinFNClient) ScriptSpent(ctx context.Context, scriptAddress, spenderAddress string) (bool, string, error) {
+	if err := client.client.ImportAddressRescan(scriptAddress, "", false); err != nil {
+		return false, "", err
+	}
+
+	if err := client.client.ImportAddressRescan(spenderAddress, "", false); err != nil {
+		return false, "", err
+	}
+
+	txs, err := client.client2.ListTransansactions()
+	if err != nil {
+		return false, "", err
+	}
+
+	var hash string
+	for _, tx := range txs {
+		if tx.Address == scriptAddress && tx.Category == "receive" {
+			hash = tx.TxID
+		}
+	}
+
+	txList, err := client.client2.ListReceivedByAddress(spenderAddress)
+	if err != nil {
+		return false, "", err
+	}
+
+	for _, txID := range txList[0].TxIDs {
+		txHash, err := chainhash.NewHashFromStr(txID)
+		if err != nil {
+			return false, "", err
+		}
+
+		tx, err := client.client.GetRawTransaction(txHash)
+		if err != nil {
+			return false, "", err
+		}
+
+		for _, txIn := range tx.MsgTx().TxIn {
+			if txIn.PreviousOutPoint.Hash.String() == hash {
+				return true, hex.EncodeToString(txIn.SignatureScript), nil
+			}
+		}
+	}
+
+	return false, "", fmt.Errorf("could not find the transaction")
+}
+
+func (client *bitcoinFNClient) PublishTransaction(ctx context.Context, stx *wire.MsgTx) (string, error) {
+	hash, err := client.client.SendRawTransaction(stx, false)
+	if err != nil {
+		return "", err
+	}
+	return hash.String(), nil
+}
+
+func (client *bitcoinFNClient) NetworkParams() *chaincfg.Params {
+	return client.params
+}
---
+++ clients/client.go
@@ -0,0 +1,77 @@
+package clients
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/btcsuite/btcd/chaincfg"
+	"github.com/btcsuite/btcd/wire"
+)
+
+type UTXO struct {
+	TxHash       string
+	Amount       int64
+	ScriptPubKey string
+	Vout         uint32
+}
+
+type ClientCore interface {
+	// NetworkParams should return the network parameters of the underlying
+	// Bitcoin blockchain.
+	NetworkParams() *chaincfg.Params
+
+	GetUTXOs(ctx context.Context, address string, limit, confitmations int64) ([]UTXO, error)
+	Confirmations(ctx context.Context, txHash string) (int64, error)
+
+	// ScriptFunded checks whether a script is funded.
+	ScriptFunded(ctx context.Context, address string, value, conf int64) (bool, int64, error)
+
+	// ScriptSpent checks whether a script is spent.
+	ScriptSpent(ctx context.Context, script, spender string) (bool, string, error)
+
+	// PublishTransaction should publish a signed transaction to the Bitcoin
+	// blockchain.
+	PublishTransaction(ctx context.Context, signedTransaction *wire.MsgTx) (string, error)
+}
+
+type Client interface {
+	ClientCore
+
+	// Balance of the given address on Bitcoin blockchain.
+	Balance(ctx context.Context, address string, confirmations int64) (int64, error)
+
+	// FormatTransactionView formats the message and txhash into a user friendly
+	// message.
+	FormatTransactionView(msg, txhash string) string
+}
+
+type client struct {
+	ClientCore
+}
+
+func NewClient(core ClientCore) Client {
+	return &client{core}
+}
+
+func (client *client) Balance(ctx context.Context, address string, confirmations int64) (int64, error) {
+	utxos, err := client.GetUTXOs(ctx, address, 999999, confirmations)
+	if err != nil {
+		return 0, nil
+	}
+	var balance int64
+	for _, utxo := range utxos {
+		balance = balance + utxo.Amount
+	}
+	return balance, err
+}
+
+func (client *client) FormatTransactionView(msg, txhash string) string {
+	switch client.NetworkParams().Name {
+	case "mainnet":
+		return fmt.Sprintf("%s, transaction can be viewed at https://www.blockchain.com/btc/tx/%s", msg, txhash)
+	case "testnet3":
+		return fmt.Sprintf("%s, transaction can be viewed at https://live.blockcypher.com/btc-testnet/tx/%s", msg, txhash)
+	default:
+		return ""
+	}
+}
